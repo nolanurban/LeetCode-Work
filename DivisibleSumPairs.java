349bd6705388c64aaa36890df7e99111ce440700
Nolan Urban <nolanurban@gmail.com>
2022-12-24 00:14:19 -0600

    More hackerrank stuff

--- DivisibleSumPairs.java
+++ DivisibleSumPairs.java
@@ -1,5 +1,6 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
  * 	Given an array of integers and a positive integer , determine the number of  pairs where  and  +  is divisible by .
@@ -25,11 +26,14 @@ public class DivisibleSumPairs {
 
 	  public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
 	         int returnInt = 0;
-	            for (int i = 0 ; i < n-1 ; i++) {
-	                for (int j = 0 ; j < n-1 ; j++) {
-	                    if (ar.get(i) > ar.get(j)) {
-	                        if ((ar.get(i) / ar.get(j)) % k == 0) {
-	                            returnInt++;
+	            for (int i = 0 ; i < n ; i++) {
+	                for (int j = 0 ; j < n ; j++) {
+	                    if (i < j) {
+	                    	int sum = 0;
+	                    	sum = ar.get(i) + ar.get(j);
+	                    	if (sum % k == 0) {
+//	                        if ((ar.get(i) + ar.get(j)) % k == 0) {
+	                        	returnInt++;
 	                        }
 	                    }
 	                }
@@ -42,13 +46,13 @@ public class DivisibleSumPairs {
 	
 	
 	public static void main(String args[]) {
-		List<Integer> testArray = new ArrayList<>();
-		testArray.add(1); 
-		testArray.add(3);
-		testArray.add(2); 
-		testArray.add(6);
-		testArray.add(1); 
-		testArray.add(2);
-		System.out.println(divisibleSumPairs(6, 3, testArray));
+		
+		String testString = "43 95 51 55 40 86 65 81 51 20 47 50 65 53 23 78 75 75 47 73 25 27 14 8 26 58 95 28 3 23 48 69 26 3 73 52 34 7 40 33 56 98 71 29 70 71 28 12 18 49 19 25 2 18 15 41 51 42 46 19 98 56 54 98 72 25 16 49 34 99 48 93 64 44 50 91 44 17 63 27 3 65 75 19 68 30 43 37 72 54 82 92 37 52 72 62 3 88 82 71";
+		String converted[] = testString.split(" ");
+		int convertedAgain[] = Arrays.stream(converted).mapToInt(Integer::parseInt).toArray();
+		List<Integer> testArray = Arrays.stream(convertedAgain).boxed().collect(Collectors.toList());
+
+
+		System.out.println("Success is: 216 return pairs -- " + divisibleSumPairs(100, 22, testArray) + " pairs");
 	}
 }
