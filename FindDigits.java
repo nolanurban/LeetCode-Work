c0b9456c57ea1509409b18475c8e5271a8db4bdd
Nolan Urban <nolanurban@gmail.com>
2023-01-17 16:04:36 -0600

    FindDigits, had a hick up and continued to decrement the original dividend -- silly mistake.

---
+++ FindDigits.java
@@ -0,0 +1,39 @@
+/* Given an integer, for each digit that makes up the integer determine whether 
+ * it is a divisor. Count the number of divisors occurring within the integer.
+ */
+public class FindDigits {
+    public static int findDigits(int n) {
+        
+    int divisorCount = 0;  // we'll use this to count the number of applicable divisors.
+    int originalNumber = n;
+    while (n > 0) {
+        int divisor = n % 10;
+        System.out.println(n + " " + divisor);
+        if (divisor != 0 &&  % divisor == 0) {
+        	System.out.println("divisible");
+        	divisorCount++;
+        }
+        n /= 10;
+    }
+    
+    return divisorCount;
+    }
+    
+    public static void main(String args[]) {
+    	int nums[] = {
+    	11,
+    	123456789,
+    	114108089,
+    	110110015,
+    	121,
+    	33,
+    	44,
+    	55,
+    	66,
+    	77,
+    	88,
+    	106108048 };
+    	for (int n : nums)
+    	System.out.println("checking digit count on " + n + " - " + findDigits(n));
+    }
+}
