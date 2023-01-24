978ddf5c42f6a119242f29c2caa807b02d9da9fb
Nolan Urban <nolanurban@gmail.com>
2023-01-24 09:22:48 -0600

    Insertion Sort complete, interesting way to decrement utilizing a solution that is O(N^2)

---
+++ InsertionSortExample.java
@@ -0,0 +1,44 @@
+/*
+ * I decided to really know sorting methods, I should work them on my own
+ * 
+ */
+public class InsertionSortExample {
+
+	public static int[] insertionSort(int nums[]) {
+		
+		for (int i = 1 ; i < nums.length ; i++) {  // we set i at 1 so we can compare 0 using j
+			int current = nums[i];
+			int last = i - 1;
+			
+			
+			while (last >= 0 && nums[last] > current) {
+				System.out.println("Switching " + nums[i] + " and " + nums[last]);
+				nums[last + 1] = nums[last]; // this is our swap out that will move our numbers in order backward
+				last = last - 1;
+			}
+			nums[last + 1] = current;
+			System.out.println("Current array: ");
+			for (int n : nums) System.out.print(n + " ");
+			System.out.println();
+
+			
+		}
+		return nums;
+		
+	}
+	public static void main(String args[]) { 
+		
+		int test[] = { 4, 2, 5, 3, 8, 11, 7, 1 };
+		// our sort method should return it as 1, 2, 3, 4, 5, 7, 8, 11 
+		for (int t : test) System.out.print(t + " ");
+		System.out.println();
+		int[] sorted = insertionSort(test);
+		System.out.println("Testing insertion sort method: ");
+		
+		
+		for (int s : sorted) System.out.print(s + " ");
+		System.out.println();
+
+		
+	}
+}
