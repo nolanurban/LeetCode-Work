322cb291740cff3c18bd03a5e176c40eecbb9af2
Nolan Urban <nolanurban@gmail.com>
2023-01-24 19:56:21 -0600

    A ton of work put into the longest palindrome -- best so far is 119/141 and I have absolute passing for every test case that seems to break LC.

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -2,34 +2,87 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LongestPalindrome {
-	   public static int longestPalindrome(String s) {
-	        Map<Character, Integer> checkMap = new HashMap<>();
-	        int charCounter = 0;
-	        int pairCount = 0;
-	        char charArray[] = s.toCharArray();
-	        int defaultValue = 1;
-	        for (char c : charArray) {
-	            if (checkMap.containsKey(c)) {
-	                pairCount++;
-	                checkMap.remove(c);
-	            }
-	            else
-	                checkMap.put(c, 1); 
-	        }
-	        if (checkMap.size() == 0) {
-	            charCounter = (pairCount * 2);
-	        }
-	        else if (checkMap.size() > 2){
-	            charCounter = (pairCount * 2) + 1;
-	        }
-	        else charCounter = (pairCount * 2) + 1;
-	        
-	        return charCounter;
-	    }
-	   public static void main(String args[]) {
-		   
-		   String test = "abccccdd";
-		   System.out.println("Testing to see the longest palindrome possible with any arrangement -- should be 7 - Result: " + longestPalindrome(test));
-		   
-	   }
+	public static String longestPalindrome(String s) {
+		int i = s.length() / 2;
+		int left = i - 1, right = i + 1;
+		char[] cArray = s.toCharArray();
+		String returnString = "";
+		returnString += s.charAt(i);
+
+		while (left >= 0 && right <= s.length()) {
+			if (cArray[left] == cArray[right]) {
+				returnString = s.substring(left, right + 1);
+				// probably need a hand off temp string
+				left--; // move our pointers
+				right++;
+			} else
+				break;
+
+		}
+		return returnString;
+	}
+
+	public static String longestPalindromeSb(String s) {
+		// same method utilizing left and right pointers, but entry begins at start
+		String returnString = "";
+		
+		if (s.length() == 1) return s;
+		
+		for (int left = 0; left < s.length(); left++) {
+			for (int right = left; right < s.length(); right++) {
+				String checkString = s.substring(left, right + 1);
+				StringBuilder sb = new StringBuilder(checkString);
+				String reversed = sb.reverse().toString();
+				System.out.println("Checking " + checkString + " against " + reversed);
+				if (checkString.equals(reversed) && checkString.length() > returnString.length())
+					returnString = checkString;
+			}
+
+		}
+		return returnString;
+	}
+	public static String longestPalindromeSbAltered(String s) {
+		// same method utilizing left and right pointers, but entry begins at start
+		String returnString = "";
+		
+		if (s.length() == 1) return s;
+		for (int left = 0; left < s.length(); left++) {
+			int right = s.length();
+//			for (int right = s.length(); right > left; right--) {
+//			System.out.println(right - left);
+			if ((right - left) < returnString.length()) {
+				System.out.println("No possible matches with a greater length palindrome, exiting");
+				break;
+			}
+			while (right > left) {
+				String checkString = s.substring(left, right);
+				String reversed = new StringBuilder(checkString).reverse().toString();
+//				System.out.println("Checking " + checkString + " against " + reversed);
+//				if (checkString.length() < returnString.length()) {
+//					System.out.println("Found palindrome larger");
+//					return returnString;
+//				}
+				right--;
+				if (reversed.charAt(0) != checkString.charAt(0)) { 
+//					System.out.println("Skipping");
+					continue;
+				}
+				if (checkString.equals(reversed) && checkString.length() > returnString.length()) {
+					returnString = checkString;
+//					System.out.println("**** palindrome: " + checkString);
+					break;
+				}
+			}
+			right = s.length();
+
+		}
+		return returnString;
+	}
+
+	public static void main(String args[]) {
+
+		String test = "twbiqwtafgjbtolwprpdnymaatlbuacrmzzwhkpvuwdtyfjsbsqxrlxxtqkjlpkvpxmlajdmnyepsmczmmfdtjfbyybotpoebilayqzvqztqgddpcgpelwmriwmoeeilpetbxoyktizwcqeeivzgxacuotnlzutdowiudwuqnghjgoeyojikjhlmcsrctvnahnoapmkcrqmwixpbtirkasbyajenknuccojooxfwdeflmxoueasvuovcayisflogtpxtbvcxfmydjupwihnxlpuxxcclbhvutvvshcaikuedhyuksbwwjsnssizoedjkbybwndxpkwcdxaexagazztuiuxphxcedqstahmevkwlayktrubjypzpaiwexkwbxcrqhkpqevhxbyipkmlqmmmogrnexsztsbkvebjgybrolttvnidnntpgvsawgaobycfaaviljsvyuaanguhohsepbthgjyqkicyaxkytshqmtxhilcjxdpcbmvnpippdrpggyohwyswuydyrhczlxyyzregpvxyfwpzvmjuukswcgpenygmnfwdlryobeginxwqjhxtmbpnccwdaylhvtkgjpeyydkxcqarkwvrmwbxeetmhyoudfuuwxcviabkqyhrvxbjmqcqgjjepmalyppymatylhdrazxytixtwwqqqlrcusxyxzymrnryyernrxbgrphsioxrxhmxwzsytmhnosnrpwtphaunprdtbpwapgjjqcnykgspjsxslxztfsuflijbeebwyyowjzpsbjcdabxmxhtweppffglvhfloprfavduzbgkw";
+		System.out.println("Result: " + longestPalindromeSbAltered(test));
+
+	}
 }
