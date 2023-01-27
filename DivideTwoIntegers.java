dec5055f98852aa024ed2e91c5434bcac47171f8
Nolan Urban <nolanurban@gmail.com>
2023-01-27 10:33:31 -0600

    worked a bit on dividing two integers not using multiplication or division -- fairly simple at first but test cases require a better understanding of bitwise operations

---
+++ DivideTwoIntegers.java
@@ -0,0 +1,31 @@
+/*
+ Given two integers dividend and divisor, divide two integers without using multiplication, division, 
+ 
+ and mod operator. The integer division should truncate toward zero, which means losing its fractional part. 
+ 
+ For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
+
+Return the quotient after dividing dividend by divisor.
+
+Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
+
+ 
+ */
+public class DivideTwoIntegers {
+    public static int divide(int dividend, int divisor) {
+    	int count = 0;
+    	
+    	while (dividend > divisor) {
+    		System.out.println(dividend);
+    		dividend -= divisor;
+    		count++;
+    	}
+    	return count;
+    }
+    public static void main(String args[]) {
+    	int dividend = 25;
+    	int divisor = 4;
+    	System.out.println("Dividing " + dividend + " / " + divisor + " without dividing or multiplying or adding: " + divide(25, 4));
+    	System.out.println("Dividing using println function: " + dividend / divisor);
+    }
+} 
